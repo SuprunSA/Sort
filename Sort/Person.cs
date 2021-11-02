8687e89fc5a4323d7e278ba3e47af0658364c9c7
Супрун Сергей <4everyongg@mail.ru>
2021-11-02 22:54:54 +0400

    Add project files.

---
+++ Sort/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sort
+{
+    public class Person : IComparable
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public int CompareTo(object o)
+        {
+            Person person = o as Person;
+            if (person != null)
+            {
+                if (Name.CompareTo(person.Name) == 0)
+                {
+                    if (Surname.CompareTo(person.Surname) == 0)
+                    {
+                        return BirthDate.CompareTo(person.BirthDate);
+                    }
+                    else return Surname.CompareTo(person.Surname);
+                }
+                else return Name.CompareTo(person.Name);
+            }
+            else throw new ArgumentException("Не явлется объектом класса Person");
+        }
+        
+        public Person(string name, string surname, DateTime dateTime)
+        {
+            Name = name;
+            Surname = surname;
+            BirthDate = dateTime;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string.Format("{0,-12}", Name));
+            sb.Append(string.Format("{0,-12}", Surname));
+            sb.Append(string.Format("{0,12:dd.MM.yyyy}", BirthDate));
+            return sb.ToString();
+        }
+    }
+}
---
+++ Sort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array.Sort(persons);
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(persons[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 7; i >= 0; i--)
+            {
+                Console.WriteLine(persons[i]);
+            }
+        }
+        
+        public static Person[] persons = new Person[8] 
+        {
+            new Person("Анастасия", "Петрова", new DateTime(1976, 2, 6)),
+            new Person("Евгения", "Ерофеева", new DateTime(1981, 3, 2)),
+            new Person("Егор", "Дементьев", new DateTime(1980, 11, 28)),
+            new Person("Ярослав", "Афанасьев", new DateTime(1979, 11, 16)),
+            new Person("Анастасия", "Петрова", new DateTime(1982, 3, 10)),
+            new Person("Виктория", "Ильина", new DateTime(1987, 5, 21)),
+            new Person("Полина", "Волкова", new DateTime(2007, 10, 4)),
+            new Person("Ярослав", "Игнатов", new DateTime(2010, 1, 12))
+        };
+    }
+}
